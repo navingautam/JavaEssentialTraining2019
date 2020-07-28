2056c21563e183d3dfdebcdbf34e3e608fe38694
gautamn <navin.gautam@tabcorp.com.au>
2020-07-28 21:35:19 +1000

    Evaluate conditions with switch-case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 
 public class Main {
@@ -8,6 +9,24 @@ public class Main {
 
         var now = LocalDateTime.now();
         var monthNumber = now.getMonthValue();
+
+        switch (monthNumber) {
+            case 1:
+                System.out.printf("The month is January");
+                break;
+            case 2:
+                System.out.printf("The month is February");
+                break;
+            case 3:
+                System.out.printf("The month is March");
+                break;
+            case 7:
+            case 8:
+                System.out.printf("It's the third quarter");
+                break;
+            default:
+                System.out.printf("You chose another month");
+        }
 
     }
 }
