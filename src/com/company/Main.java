a621a307efc45d7f50bf47870a6b3d731e183f43
gautamn <navin.gautam@tabcorp.com.au>
2020-07-28 23:10:26 +1000

    Handle exceptions with try/catch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String s = null;
+        try {
+            var sub = s.substring(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.printf("I'm not dead yet!");
     }
 }
+
